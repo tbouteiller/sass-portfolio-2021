831f7568b0f77917611dc316bfb101c6f32c27de
Tanner <tannerbouteiller@gmail.com>
2021-08-08 21:10:36 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  prevScrollpos > currentScrollPos
+    ? (document.getElementById("navbar").style.top = "0")
+    : (document.getElementById("navbar").style.top = "-80px");
+  prevScrollpos = currentScrollPos;
+};
